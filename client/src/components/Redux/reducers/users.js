3d8c9834053b4cfd84fa4b884be6742d7a178af6
vishalverma1999 <vermavishal1999.vv@gmail.com>
2021-11-10 13:12:12 +0530

    Creating defaultState of reducer

---
+++ client/src/components/Redux/reducers/users.js
@@ -0,0 +1,12 @@
+const defaultState = {
+    all: {},
+    users: {},
+    currentUser: "",
+    email: "",
+    isDataInitialized: false,
+    bookings: [],
+    isLoading: false,
+    loggedin: false,
+    registeredUser: false,
+    registeredUserData: {},
+  };
