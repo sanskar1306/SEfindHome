badfa6ce6b3146ac49c56b8dc4c4c552142a500d
vishalverma1999 <vermavishal1999.vv@gmail.com>
2021-11-10 12:38:48 +0530

    Redux store created

---
+++ client/src/components/Redux/store.js
@@ -0,0 +1,5 @@
+import thunkMiddleware from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "../Redux/reducers/users";
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export default store;
