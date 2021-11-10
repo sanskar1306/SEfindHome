b5c61435096d4efbbc6d34ab7929c127adaa7a24
sanskar1306 <sanskarkabra1306@gmail.com>
2021-11-10 09:08:58 +0530

    main component added

---
+++ client/src/components/main.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export default function MainComponent() {
+  return (
+    <>
+     
+      <div style={{ height: 50 }}> Main Component</div>
+      
+    </>
+  );
+}
