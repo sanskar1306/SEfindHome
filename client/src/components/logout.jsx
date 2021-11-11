fc99110487cb5b4311e02b206107859510effcf6
sanskar1306 <sanskarkabra1306@gmail.com>
2021-11-11 22:05:25 +0530

    Login and logout component created

---
+++ client/src/components/logout.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { GoogleLogout } from "react-google-login";
+import { connect } from "react-redux";
+
+class Logout extends Component {
+    onSuccess = () => {
+        // alert("logout success");
+        this.props.log();
+    };
+
+    onFail = () => {
+        alert("logout Failed");
+    };
+    render() {
+        return (
+            <div>
+                <GoogleLogout
+                    clientId="637286602051-he39r2u56o4ks6a9g0e38mtu6rro8hlr.apps.googleusercontent.com"
+                    buttonText="Signout"
+                    onLogoutSuccess={this.onSuccess}
+                    onFailure={this.onFail}
+                    theme="dark"
+                />
+            </div>
+        );
+    }
+}
+
+const mapDispatchtoProps = (dispatch) => {
+    return {
+        log: () => dispatch({ type: "LOGOUT" }),
+    };
+};
+
+export default connect(null, mapDispatchtoProps)(Logout);
