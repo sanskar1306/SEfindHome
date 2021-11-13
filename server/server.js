14b1d903465101def9281af4cd69f8907a4145db
Namanl2001 <namanlakhwani@gmail.com>
2021-11-13 18:01:03 +0530

    adding admin guest route
    Signed-off-by: Namanl2001 <namanlakhwani@gmail.com>

--- server/server.js
+++ server/server.js
@@ -61,7 +61,11 @@ const guestRouter = require("./routes/guest");
 //User Routes
 app.use("/users", usersRouter);
 
+//Admin Routes
+app.use("/admin", adminRouter);
 
+//Guest Routes
+app.use("/guest", guestRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
