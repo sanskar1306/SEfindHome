5a432a27b93661ff1c6168369bcb78222a7bf556
vishalverma1999 <vermavishal1999.vv@gmail.com>
2021-11-16 21:39:49 +0530

    Adding Card Header and Image

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 import { Card, Button } from 'bootstrap-4-react';
 
 
-    class card extends Component {
+class card extends Component {
     render() {
         return (
-            <div>
-                
+            <div className='box-container'>
+                <div className='box-item'>
+                    <Card style={{ width: '18rem', height: "30rem" }} className='flip-box'>
+                        <Card.Header>{this.props.user.name}</Card.Header>
+                        <Card.Image src={imageUrl} />
+                    </Card>
+                </div>
             </div>
         )
     }
