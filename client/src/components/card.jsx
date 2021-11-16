5316f65fa86e65b5b6413147af0cae95c7e080a7
vishalverma1999 <vermavishal1999.vv@gmail.com>
2021-11-16 21:51:25 +0530

    Adding handleclick and handleclick1 functions to make card functional

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -5,6 +5,63 @@ import { Card, Button } from 'bootstrap-4-react';
 import img_src from './im'
 
 class card extends Component {
+
+    handleClick = () => {
+        // console.log(this.props.user._id.toString());
+        axios
+            .post(`https://find-home2021.herokuapp.com/users/book/${this.props.user._id}/${1}`)
+            .then(
+                res => {
+                    console.log(res)
+                    axios
+                        .post(`https://find-home2021.herokuapp.com/guest/add/${this.props.user._id}/`, {
+                            email: this.props.email
+                        })
+                        .then(
+                            res => {
+                                console.log(res);
+                                window.location.reload();
+                            },
+                            error => {
+                                console.log(`Error: ${error}`);
+                            }
+                        );
+                },
+                error => {
+
+                    console.log(`Error: ${error}`);
+                },
+
+            );
+
+    };
+
+    handleClick1 = () => {
+        console.log(this.props.user._id.toString());
+        axios
+            .post(`https://find-home2021.herokuapp.com/users/book/${this.props.user._id}/${0}`)
+            .then(
+                res => {
+
+                    axios
+                        .delete(`https://find-home2021.herokuapp.com/guest/delete/${this.props.user._id}/`)
+                        .then(
+                            res => {
+                                console.log(res);
+                                window.location.reload();
+                            },
+                            error => {
+                                console.log(`Error: ${error}`);
+                            }
+                        );
+                },
+                error => {
+                    console.log(`Error: ${error}`);
+                }
+            );
+
+    };
+
     render() {
         var imageUrl = '';
 
