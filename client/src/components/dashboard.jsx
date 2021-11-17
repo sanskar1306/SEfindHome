c61745f764d8b88db106680caa1acde23f0b2db5
vishalverma1999 <vermavishal1999.vv@gmail.com>
2021-11-17 22:39:58 +0530

    Importing Progress Bar From Bootstrap and using bootstrap classes to use it effectively

--- client/src/components/dashboard.jsx
+++ client/src/components/dashboard.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
+import { Progress } from 'bootstrap-4-react';
 
 class dashboard extends Component {
 
     constructor(props) {
         super(props);
         this.state = { guests: [] };
-       
+
     }
-    
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <>
+                <div>
+
+                    <br />
+                    <br />
+                </div>
+
+                {!this.props.isDataInitialized && (
+                    <Progress mb='4' w='25%'>
+                        <Progress.Bar striped animated min='0' max='100' mx='auto' now='50'>
+                            Initializing data
+                        </Progress.Bar>
+                    </Progress>
+                )}
+
+            </>
         )
     }
 }
