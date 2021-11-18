788f800dc868a01389d686220ec0fb2973a15d2a
sanskar1306 <sanskarkabra1306@gmail.com>
2021-11-18 10:50:12 +0530

    filter component initialized

---
+++ client/src/components/filter.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './style.css';
+import { Dropdown, Button } from 'bootstrap-4-react';
+
+class filter extends Component {
+
+
+    render() {
+        return (
+            <div>
+              
+            </div>
+        );
+    }
+}
+
+export default filter;
+
+
+
